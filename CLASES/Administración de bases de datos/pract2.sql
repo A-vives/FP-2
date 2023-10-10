3af24ccf05f09a121b2d91b1cfd667ba94c4298e
A-vives <avives@cifpfbmoll.eu>
2023-10-10 21:12:42 +0200

    s

--- CLASES/Administración de bases de datos/pract2.sql
+++ CLASES/Administración de bases de datos/pract2.sql
@@ -1,16 +1,7 @@
 3-------------------
-
-    CREATE TABLE country (
-  country_id SERIAL(15) PRIMARY KEY,
-  name character varying(15),
-  region_id character varying(25),
-  FOREIGN KEY (region_id) REFERENCES region(region_id)
-)
-
-ALTER TABLE ONLY orders
-    ADD CONSTRAINT region_id PRIMARY KEY (region_id);
-
-country_id  (PK),
- country_name 
- region_id 
- (FK de Region)
+create table countries (
+  countries_id varchar(25) primary key,
+  countries_name varchar(50),
+  region_id int,
+  foreign key (region_id) references region(region_id)
+);
